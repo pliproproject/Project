8359942fb54b2603e76e28ed10c42d9d41f677a9
Alex <manousale@gmail.com>
2022-03-24 11:39:18 +0200

    before changing frames (hide/show)

--- high_scores.py
+++ high_scores.py
@@ -4,7 +4,8 @@ from tkinter import ttk
 def show_splash_screen(parent):
     lbl_splash = ttk.Label(parent, text='splash screen', font='Arial 16 bold')
     lbl_splash.place(x=100, y=30)
-    lbl_splash.after(2000, lbl_splash.destroy)
+
+#    lbl_splash.after(2000, lbl_splash.destroy)
 
 
 def show_high_scores(parent):
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from time import sleep
 from get_questions import *
 from high_scores import *
 from play import *
@@ -7,9 +8,14 @@ from play import *
 appname = "doYouKnow?"
 
 
+def exit_splash():
+    frame_splash.place_forget()
+    frame_hi_scores.place
+
+
 def start_game():
-    print("game started")
-    frame_main.place_forget()
+    frame_hi_scores.pack_forget()
+    frame_play.place
     get_name(frame_top)
     get_category(frame_top)
     get_difficulty(frame_top)
@@ -17,7 +23,7 @@ def start_game():
 
 def play_trivial():
     qa = get_questions(1, 1)
-    play(qa, frame_main)
+    play(qa, frame_play)
 
 
 # ----------------- main --------------------------
@@ -47,22 +53,31 @@ if __name__ == '__main__':
 
     frame_top = tk.Frame(root, bg='grey')
     frame_top.place(y=1, height=100, width=window_width)
-    frame_top.pack
 
-    frame_main = tk.Frame(root, bg='white')
-    frame_main.place(y=100, height=window_height - 160, width=window_width)
-    frame_main.pack
+    frame_splash = tk.Frame(root, bg='white')
+    frame_splash.place(y=100, height=window_height - 160, width=window_width)
+
+    frame_hi_scores = tk.Frame(root, bg='red')
+    frame_hi_scores.place(y=100, height=window_height - 160, width=window_width)
+    #  frame_hi_scores.place_forget()
+
+    frame_play = tk.Frame(root, bg='green')
+    frame_play.place(y=100, height=window_height - 160, width=window_width)
+    # frame_play.place_forget()
 
     frame_bottom = tk.Frame(root, bg='grey')
     frame_bottom.place(y=708, height=60, width=window_width)
-    frame_bottom.pack
 
     title = ttk.Label(frame_top, text=appname, font='Arial 18 bold')
     title.place(x=20, y=1)
     # Show splash screen for a couple of seconds or destroy on click
-    show_splash_screen(frame_main)
+
+    show_splash_screen(frame_splash)
     # Show high scores
-    show_high_scores(frame_main)
+    #  frame_splash.pack_forget()
+    #  frame_hi_scores
+    #   frame_hi_scores.pack
+    #    show_high_scores(frame_hi_scores)
 
     # start button
     btn_start = ttk.Button(frame_bottom, text="Enter name, category, difficulty", command=start_game)
@@ -71,6 +86,10 @@ if __name__ == '__main__':
     # play button
     btn_play = ttk.Button(frame_bottom, text="Start Game", command=play_trivial)
     btn_play.place(x=200, y=20)
+
+    # ok button
+    btn_exit_splash = ttk.Button(frame_bottom, text="ok", command=exit_splash)
+    btn_exit_splash.place(x=300, y=20)
 
     # exit button
     btn_exit = ttk.Button(frame_bottom, text='Exit', command=lambda: root.quit())
