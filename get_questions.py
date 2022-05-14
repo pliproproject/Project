736ee908e8088a108a97fdeadacdec650af1f9ed
nicks <nick.semertz@gmail.com>
2022-05-14 20:57:03 +0300

    Τελικός Κώδικας

--- get_questions.py
+++ get_questions.py
@@ -1,61 +1,171 @@
-from tkinter import ttk
-from urllib import response
+from tkinter import *
+from tkinter.ttk import *
 import requests
-import json
+
+url = "https://opentdb.com/api.php"
+
+username = ""
+
+level = ""
+
+# Πρώτο παράθυρο για λήψη ονόματος παίκτη
+window = Tk()
+
+window.title("Username")
+
+window.geometry('200x200')
+
+lbl = Label(window, text="Please type a username:")
+
+lbl.grid(column=0, row=0)
+
+txt = Entry(window, width=10)
+
+txt.grid(column=1, row=0)
 
 
-def get_questions(category, difficulty):
-    qa = [
-        {"question": "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
-         "correct_answer": "Apple", "incorrect_answers": ["Microsoft", "Atari", "Commodore"]},
-        {"question": "In any programming language, what is the most common way to iterate through an array?",
-         "correct_answer": "For loops",
-         "incorrect_answers": ["If Statements", "Do-while loops", "While loops"]},
-        {"question": "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
-         "correct_answer": "1000", "incorrect_answers": ["512", "1024", "500"]},
-        {"question": "When Gmail first launched, how much storage did it provide for your email?",
-         "correct_answer": "1GB",
-         "incorrect_answers": ["512MB", "5GB", "Unlimited"]},
-        {"question": "In the programming language Java, which of these keywords would you put on a variable to "
-                     "make sure it does&#039;t get modified?", "correct_answer": "Final",
-         "incorrect_answers": ["Static", "Private", "Public"]},
-        {"question": "The logo for Snapchat is a Bell.", "correct_answer": "False", "incorrect_answers": ["True"]},
-        {"question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
-        {"question": "Time on Computers is measured via the EPOX System.", "correct_answer": "False",
-         "incorrect_answers": ["True"]},
-        {"question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
-         "correct_answer": "False", "incorrect_answers": ["True"]}]
-    return qa
+def clicked():
+    username = txt.get()
+
+    window.destroy()
 
 
-def get_user_data(parent):
-    player = input("Δώσε το όνομά σου: ")
+btn = Button(window, text="Continue", command=clicked)
 
-    # Κατηγορία
-    category_link = requests.get('https://opentdb.com/api_category.php')
-    print(category_link.json())
+btn.grid(column=0, row=2)
 
-    category = int(input('Επέλεξε κατηγορία ερωτήσεων 9 - 32: '))
+window.mainloop()
 
-    id_ = 0
-    if category >= 9:
-        if category <= 32:
-            id_ = category
+# --------------------------------------------------
 
-    # Επίπεδο Δυσκολίας
-    difficulty = input('Επέλεξε βαθμό δυσκολίας [Easy], [Medium], [Hard]: ')
-    if difficulty == 'Easy':
-        difficulty = 'easy'
-    elif difficulty == 'Medium':
-        difficulty = 'medium'
-    elif difficulty == 'Hard':
-        difficulty = 'hard'
+# Δεύτερο παράθυρο επιλογής επιπέδου δυσκολίας
 
-    # Τελική μορφή API Link
-    api_link = 'https://opentdb.com/api.php?amount=9&category='
-    print(api_link + str(id_) + '&difficulty=' + str(difficulty))
+window2 = Tk()
 
-    lbl_player = ttk.Label(parent, text='get user name, category, difficulty frame', font='Arial 12 bold')
-    lbl_player.place(x=10, y=10)
+window2.title("Select Level")
 
-    return [player, category, difficulty]
+window2.geometry('200x200')
+
+var = StringVar()
+
+var.set(None)
+
+
+def selected():
+    nlevel = int(var.get())
+
+    if (nlevel == 1):
+        level = "easy"
+    if (nlevel == 2):
+        level = "medium"
+    if (nlevel == 3):
+        level = "hard"
+
+
+rad1 = Radiobutton(window2, text='Easy   ', value=1, variable=var, command=selected)
+
+rad2 = Radiobutton(window2, text='Medium', value=2, variable=var, command=selected)
+
+rad3 = Radiobutton(window2, text='Hard   ', value=3, variable=var, command=selected)
+
+rad1.grid(column=1, row=1)
+
+rad2.grid(column=1, row=2)
+
+rad3.grid(column=1, row=3)
+
+var.set(1)
+
+
+def clicked2():
+    window2.destroy()
+
+
+btn2 = Button(window2, text="Continue", command=clicked2)
+
+btn2.grid(column=1, row=4)
+
+window2.mainloop()
+
+# ------------------------------------------------------------------------
+
+# Τρίτο παράθυρο επιλογής κατηγορίας ερωτήσεων
+
+window3 = Tk()
+
+window3.title("Select Category")
+
+window3.geometry('150x150')
+
+lbl = Label(window3, text="Please select a category:")
+
+lbl.grid(column=0, row=0)
+
+cat = StringVar()
+cat_cb = Combobox(window3, textvariable=cat)
+
+# prevent typing a value
+cat_cb['state'] = 'readonly'
+
+cat_cb['values'] = ('Any Category',
+                    'General Knowledge',
+                    'Entertainment: Books',
+                    'Entertainment: Film',
+                    'Entertainment: Music',
+                    'Entertainment: Musicals & Theatres',
+                    'Entertainment: Television',
+                    'Entertainment: Video Games',
+                    'Entertainment: Board Games',
+                    'Science & Nature',
+                    'Science: Computers',
+                    'Science: Mathematics',
+                    'Mythology',
+                    'Sports',
+                    'Geography',
+                    'History',
+                    'Politics',
+                    'Art',
+                    'Celebrities',
+                    'Animals',
+                    'Vehicles',
+                    'Entertainment: Comics',
+                    'Science: Gadgets',
+                    'Entertainment: Japanese Anime & Manga',
+                    'Entertainment: Cartoon & Animations')
+
+cat_cb.current(0)
+cat_cb.grid(column=0, row=2)
+
+
+def clicked3():
+    if (cat_cb.current() != 0):
+        what_category = 8 + cat_cb.current()
+    else:
+        what_category = "any"
+
+    if (what_category == "any"):
+        params = dict(
+            amount='9',
+            difficulty=level
+        )
+    else:
+        params = dict(
+            amount='9',
+            category=what_category,
+            difficulty=level
+        )
+    # Εκτελεί την αίτηση GET προς τον δικτυακό τόπο opentdb.com
+    resp = requests.get(url=url, params=params)
+    # Επιστρέφει την απάντηση της αίτησης (request) σε μορφή dict
+    data = resp.json()
+
+    print(data)
+    window3.destroy()
+
+
+btn3 = Button(window3, text="Continue", command=clicked3)
+
+btn3.grid(column=0, row=5)
+
+window3.mainloop()
+# -------------------------------------------------------------------------
