99fac0bb844bc5f1eb3adf5c1dc835dd0bffef5a
Alex <manousale@gmail.com>
2022-05-19 19:56:09 +0300

    Διαβάζει τις επιλογές του παίχτη και φέρνει ερωτήσεις από την opentdb. Μεταφορά της δημιουργίας των frames στη main

--- game_start.py
+++ game_start.py
@@ -7,42 +7,44 @@ username = ""
 level = ""
 
 
-def get_user_data(parent,frame_play, frame_top, frame_bottom, frame_game_score):
-#λήψη ονόματος παίκτη
+def get_user_data(parent, frame_play, frame_top, frame_bottom, frame_game_score):
+    # λήψη ονόματος παίκτη
     lbl = Label(parent, text="Please type a username:")
-    #lbl.grid(column=0, row=0)
+    # lbl.grid(column=0, row=0)
     lbl.place(x=150, y=30)
-    txt = Entry(parent,width=10)
+    txt = Entry(parent, width=10)
     txt.place(x=300, y=30)
-    #txt.grid(column=1, row=0)
+    # txt.grid(column=1, row=0)
 
-#επιλογή επιπέδου δυσκολίας
+    # επιλογή επιπέδου δυσκολίας
     var = StringVar()
     var.set(None)
+
     def selected():
         nlevel = int(var.get())
-        if (nlevel==1):
-            level="easy"
-        if (nlevel==2):
-            level="medium"
-        if (nlevel==3):
-            level="hard"
-    Label(parent, text="select level:").place(x=150,y=70)
-    rad1 = Radiobutton(parent,text='Easy   ', value=1, variable=var, command=selected)
-    rad2 = Radiobutton(parent,text='Medium', value=2, variable=var, command=selected)
-    rad3 = Radiobutton(parent,text='Hard   ', value=3, variable=var, command=selected)
+        if (nlevel == 1):
+            level = "easy"
+        if (nlevel == 2):
+            level = "medium"
+        if (nlevel == 3):
+            level = "hard"
 
-    rad1.place(x=300,y=70)
-    rad2.place(x=300,y=90)
-    rad3.place(x=300,y=110)
+    Label(parent, text="select level:").place(x=150, y=70)
+    rad1 = Radiobutton(parent, text='Easy   ', value=1, variable=var, command=selected)
+    rad2 = Radiobutton(parent, text='Medium', value=2, variable=var, command=selected)
+    rad3 = Radiobutton(parent, text='Hard   ', value=3, variable=var, command=selected)
+
+    rad1.place(x=300, y=70)
+    rad2.place(x=300, y=90)
+    rad3.place(x=300, y=110)
     # rad1.grid(column=1, row=1)
     # rad2.grid(column=1, row=2)
     # rad3.grid(column=1, row=3)
 
-#επιλογή κατηγορίας ερωτήσεων
+    # επιλογή κατηγορίας ερωτήσεων
     lbl = Label(parent, text="Please select a category:")
-    #lbl.grid(column=0, row=0)
-    lbl.place(x=150,y=140)
+    # lbl.grid(column=0, row=0)
+    lbl.place(x=150, y=140)
     cat = StringVar()
     cat_cb = Combobox(parent, textvariable=cat)
 
@@ -75,40 +77,40 @@ def get_user_data(parent,frame_play, frame_top, frame_bottom, frame_game_score):
                         'Entertainment: Cartoon & Animations')
 
     cat_cb.current(0)
-    cat_cb.place(x=300,y=140)
-   # cat_cb.grid(column=0, row=2)
-                    
+    cat_cb.place(x=300, y=140)
+
+    # cat_cb.grid(column=0, row=2)
 
     def clicked3():
         username = txt.get()
-       # print('user=',username)
+        # print('user=',username)
         nlevel = int(var.get())
-       # print('category=',cat_cb.current())
-        if (nlevel==1):
-            level="easy"
-        if (nlevel==2):
-            level="medium"
-        if (nlevel==3):
-            level="hard"
-        print('level=',level)
-        if (cat_cb.current()!=0):
+        # print('category=',cat_cb.current())
+        if (nlevel == 1):
+            level = "easy"
+        if (nlevel == 2):
+            level = "medium"
+        if (nlevel == 3):
+            level = "hard"
+        print('level=', level)
+        if (cat_cb.current() != 0):
             what_category = 8 + cat_cb.current()
         else:
             what_category = "any"
         if (what_category == "any"):
             params = dict(
-            amount='9',
-            difficulty=level
+                amount='9',
+                difficulty=level
             )
         else:
             params = dict(
-            amount='9',
-            category=what_category,
-            difficulty=level
+                amount='9',
+                category=what_category,
+                difficulty=level
             )
-    # καλεί την play για να αρχίσει το παιχνίδι
-        play(frame_play, frame_top, frame_bottom, frame_game_score, parent, params,username)
+        # καλεί την play για να αρχίσει το παιχνίδι
+        play(frame_play, frame_top, frame_bottom, frame_game_score, parent, params, username)
 
     btn3 = Button(parent, text="Continue", command=clicked3)
-    btn3.place(x=350,y=200)
-    #btn3.grid(column=0, row=5)
+    btn3.place(x=350, y=200)
+    # btn3.grid(column=0, row=5)
--- get_questions.py
+++ get_questions.py
@@ -1,6 +1,7 @@
 import requests
-def get_questions(url,params):
 
+
+def get_questions(url, params):
     qa = [
         {"question": "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
          "correct_answer": "Apple", "incorrect_answers": ["Microsoft", "Atari", "Commodore"]},
@@ -21,13 +22,12 @@ def get_questions(url,params):
          "incorrect_answers": ["True"]},
         {"question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
          "correct_answer": "False", "incorrect_answers": ["True"]}]
-    #Εκτελεί την αίτηση GET προς τον δικτυακό τόπο opentdb.com
+    # Εκτελεί την αίτηση GET προς τον δικτυακό τόπο opentdb.com
     resp = requests.get(url=url, params=params)
-    #Επιστρέφει την απάντηση της αίτησης (request) σε μορφή dict
+    # Επιστρέφει την απάντηση της αίτησης (request) σε μορφή dict
     data = resp.json()
 
     print(data['results'])
-  #  play(qa, frame_play, frame_top, frame_bottom, frame_game_score)
-  #  return data['results']
+    #  play(qa, frame_play, frame_top, frame_bottom, frame_game_score)
+    #  return data['results']
     return data
-
